d3387c084b381ace21ba33d109cfc40d001c5cd5
Andrei <n3rd4i@gmail.com>
2019-05-19 20:42:54 +0200

    initial version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installLocation = "$ENV:LocalAppData\Programs\Doomseeker"
+$startMenuLocation = "$ENV:AppData\Microsoft\Windows\Start Menu\Programs\Doomseeker"
+New-Item -ItemType Directory -Force -Path $installLocation
+New-Item -ItemType Directory -Force -Path $startMenuLocation
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $installLocation
+  url           = 'http://doomseeker.drdteam.org/files/doomseeker-1.2_windows.zip'
+  softwareName  = 'doomseeker*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = '461066E4F3AB85A5B0CBED442201B877'
+  checksumType  = 'md5' #default is md5, can also be sha1, sha256 or sha512
+}
+Install-ChocolateyZipPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+
+Install-ChocolateyShortcut `
+  -ShortcutFilePath "$startMenuLocation\Doomseeker.lnk" `
+  -TargetPath "$installLocation\Doomseeker.exe" `
+  -IconLocation "$installLocation\Doomseeker.exe" `
+  -Description "Doomseeker version 1.2.0" `
+  -WorkingDirectory "$installLocation"
+
+Install-ChocolateyShortcut `
+  -ShortcutFilePath "$ENV:UserProfile\Desktop\Doomseeker.lnk" `
+  -TargetPath "$installLocation\Doomseeker.exe" `
+  -IconLocation "$installLocation\Doomseeker.exe" `
+  -Description "Doomseeker version 1.2.0" `
+  -WorkingDirectory "$installLocation"
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,36 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$installLocation = "$ENV:LocalAppData\Programs\Doomseeker"
+$startMenuLocation = "$ENV:AppData\Microsoft\Windows\Start Menu\Programs\Doomseeker"
+$shortcutPath = "$ENV:UserProfile\Desktop\Doomseeker.lnk"
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'ZIP'
+  softwareName  = 'doomseeker*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  zipFileName   = 'doomseeker-1.2_windows.zip'
+}
+Remove-Item $installLocation -recurse -force
+Remove-Item $startMenuLocation -force
+Remove-Item $shortcutPath -force
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $packageArgs['file'] = "$($_.UninstallString)" #NOTE: You may need to split this if it contains spaces, see below
+    
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    } else {
+    }
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
